001ad70f1c324f06ed382fd716ba6ff81734c75c
Gideon I. Glass <gidglass@gmail.com>
2016-04-06 13:39:13 -0400

    Added dropdown actions that trigger modal

--- client/components/Modal.jsx
+++ client/components/Modal.jsx
@@ -4,17 +4,18 @@ import React, { PropTypes } from 'react'; // eslint-disable-line no-unused-vars
 import ModalHeader from '../components/ModalHeader';
 import ModalBody from '../components/ModalBody';
 
-export const Modal = ({ children }) => (
+export const Modal = ({ title, children }) => (
   <div id="modal" className="modal fade" role="dialog">
     <div className="modal-dialog">
       <div className="modal-content">
-        <ModalHeader title="Add Donation" />
+        <ModalHeader title={title} />
         <ModalBody>{children}</ModalBody>
       </div>
     </div>
   </div>);
 
 Modal.propTypes = {
+  title: PropTypes.string,
   children: PropTypes.node,
 };
 
--- client/components/Profile/Avatar.jsx
+++ client/components/Profile/Avatar.jsx
@@ -8,19 +8,36 @@ export const Avatar = ({ children }) =>
   <section className="avatar">
     <div className="profile-img"></div>
     <h1>{children}</h1>
-    <Button className="btn" toggle="modal" target="#modal">Add Donation</Button>
-    <Modal>
-            <form role="form">
-              <div className="form-group">
-                <label htmlFor="amount">Amount</label>
-                <input type="number" min="1" step="any" className="form-control" id="amount" />
-              </div>
-              <div className="form-group">
-                <label htmlFor="date">Date</label>
-                <input type="date" className="form-control" id="date" />
-              </div>
-              <button type="submit" className="btn btn-primary">Save</button>
-            </form>
+    <div className="dropdown">
+      <Button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenu">
+        Add New &nbsp;
+        <span className="caret"></span>
+      </Button>
+      <ul className="dropdown-menu">
+        <li>
+          <a href="#" data-toggle="modal" data-target="#modal">Donation</a>
+        </li>
+        <li>
+          <a href="#" data-toggle="modal" data-target="#modal">Event</a>
+        </li>
+        <li>
+          <a href="#" data-toggle="modal" data-target="#modal">Note</a>
+        </li>
+      </ul>
+    </div>
+    <Modal title="Add Donation">
+      {/* TODO: Render different form based on button clicked */}
+      <form role="form">
+        <div className="form-group">
+          <label htmlFor="amount">Amount</label>
+          <input type="number" min="1" step="any" className="form-control" id="amount" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="date">Date</label>
+          <input type="date" className="form-control" id="date" />
+        </div>
+        <button type="submit" className="btn btn-primary">Save</button>
+      </form>    
     </Modal>
   </section>;
 
