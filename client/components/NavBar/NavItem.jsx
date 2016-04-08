5962a1dce0297a4a74b62f1e7d2eda509472b038
Gideon I. Glass <gidglass@gmail.com>
2016-04-07 20:12:14 -0400

    added dashboard template

--- client/components/NavBar/NavItem.jsx
+++ client/components/NavBar/NavItem.jsx
@@ -3,7 +3,7 @@
 import React, { PropTypes } from 'react'; // eslint-disable-line no-unused-vars
 
 export const NavItem = ({ children }) =>
-  <li><a href="#">{children}</a></li>;
+  <li>{children}</li>;
 
 NavItem.propTypes = {
   children: PropTypes.object,
--- client/pages/Dashboard.jsx
+++ client/pages/Dashboard.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import NavBar from '../components/NavBar';
+import SideBar from '../components/NavBar/SideBar';
 
 export const Dashboard = () =>
-  <div>TODO: make a beautiful dashboard</div>;
+  <div>
+    <NavBar brandName="Mochi" />
+    <SideBar />
+    <section id="main-container" className="container-fluid">
+      <div className="row">
+        <div className="col-md-7 col-md-offset-1 main profile">
+          <h1>Dashboard</h1>
+        </div>
+      </div>
+    </section>
+  </div>;
 
 export default Dashboard;
