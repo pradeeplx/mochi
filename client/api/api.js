caae76b45fa236fd771b14d62af3395758cc2e00
Sean Herman <seanherman@gmail.com>
2016-04-07 20:24:51 -0400

    actual API call

--- client/api/api.js
+++ client/api/api.js
@@ -1,22 +1,21 @@
-// import fetch from 'whatwg-fetch';
+import 'whatwg-fetch';
 
-const testUser = {
-  profile: {
-    email: 'sjh293@cornell.edu',
-    twitter: '@seannnnnnnnnnnn',
-    phone: '3479850861',
-    city: 'New York',
-    state: 'NY',
-    first: 'Sean',
-    last: 'Herman',
-  },
-  events: [
-  ],
-};
+// const testUser = {
+//   profile: {
+//     email: 'sjh293@cornell.edu',
+//     twitter: '@seannnnnnnnnnnn',
+//     phone: '3479850861',
+//     city: 'New York',
+//     state: 'NY',
+//     first: 'Sean',
+//     last: 'Herman',
+//   },
+//   events: [
+//   ],
+// };
 
-export const getUser = () => {
-  return testUser;
-  // fetch(`/user/${username}`)
-  // .then(res => res.json())
-  // .catch(err => console.error(err));
-};
+export const getUser = (username) =>
+  // return testUser;
+  fetch(`/users/${username}`)
+  .then(res => res.json())
+  .catch(err => console.error(err));
