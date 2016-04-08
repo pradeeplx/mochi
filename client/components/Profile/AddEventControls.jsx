6119c1a627eac1c1d9cbd665c982c044f5fc4eb4
Gideon I. Glass <gidglass@gmail.com>
2016-04-08 13:50:33 -0400

    Recomponentized

---
+++ client/components/Profile/AddEventControls.jsx
@@ -0,0 +1,35 @@
+'use strict';
+
+import React, { PropTypes } from 'react'; // eslint-disable-line no-unused-vars
+import Dropdown from '../../components/Button/Dropdown';
+import Modal from '../Modal';
+
+export class AddEventControls extends React.Component {
+  renderModal() {
+    /* TODO: Render different form based on button clicked */
+    return;
+  }
+  render() {
+    return (
+      <div className="event-controls">
+        <Modal title="Add Donation">
+          {/* TODO: Render different form based on button clicked */}
+          <form role="form">
+            <div className="form-group">
+              <label htmlFor="amount">Amount</label>
+              <input type="number" min="1" step="any" className="form-control" id="amount" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="date">Date</label>
+              <input type="date" className="form-control" id="date" />
+            </div>
+            <button type="submit" className="btn btn-primary">Save</button>
+          </form>
+        </Modal>
+        <Dropdown id="addItem" title="Add New" />
+      </div>
+    );
+  }
+}
+
+export default AddEventControls;
