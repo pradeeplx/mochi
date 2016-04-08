192081fd126189c3216e044822f07f1c535e28de
Gideon I. Glass <gidglass@gmail.com>
2016-04-08 14:20:16 -0400

    fixing stuff

--- client/components/DonorRow.jsx
+++ client/components/DonorRow.jsx
@@ -1,12 +1,15 @@
 'use strict';
 
-import React from 'react'; // eslint-disable-line no-unused-vars
+import React, { PropTypes } from 'react'; // eslint-disable-line no-unused-vars
 
 export const DonorRow = () =>
-	<tr>
-		<td>Firstname</td>
-   	<td>Lastname</td>
-   	<td>Email</td>
-	</tr>
+  <tr>
+    <td>Firstname</td>
+      <td>Lastname</td>
+      <td>Email</td>
+  </tr>;
+
+DonorRow.propTypes = {
+};
 
 export default DonorRow;
--- client/components/DonorTable.jsx
+++ client/components/DonorTable.jsx
@@ -1,18 +1,30 @@
 'use strict';
 
-import React from 'react'; // eslint-disable-line no-unused-vars
+import React, { PropTypes } from 'react'; // eslint-disable-line no-unused-vars
 import DonorRow from '../components/DonorRow';
 
-export const DonorTable = () =>
-  <table className="table table-hover">
-     <thead>
-      <DonorRow />
-     </thead>
-     <tbody>
-      <DonorRow />
-      <DonorRow />
-      <DonorRow />
-     </tbody>
-  </table>;
+export const DonorTable = ({ users = [] }) => {
+  const userElements = users.map(user =>
+    <DonorRow first={user.first} last={user.last} email={user.email} city={user.city} username={user.username} />
+  );
+  return (
+    <table className="table table-hover">
+       <thead>
+          <tr>
+            <th>First</th>
+            <th>Last</th>
+            <th>Email</th>
+            <th>City</th>
+          </tr>
+       </thead>
+       <tbody>
+        {userElements}
+       </tbody>
+    </table>);
+};
+
+DonorTable.propTypes = {
+  users: PropTypes.arrayOf(PropTypes.object),
+};
 
 export default DonorTable;
